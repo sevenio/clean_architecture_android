abcc4851ff491357877dc77f968aade85fec7201
sevenio <seveniotacheon@gmail.com>
2018-08-08 05:04:52 +0530

    build get projects use case

---
+++ Domain/src/main/java/com/example/domain/interactor/GetProjects.kt
@@ -0,0 +1,14 @@
+package com.example.domain.interactor
+
+import com.example.domain.executor.PostExecutionThread
+import com.example.domain.model.Project
+import com.example.domain.repository.ProjectsRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class GetProjects @Inject constructor(private val projectsRepository: ProjectsRepository, postExecutionThread: PostExecutionThread): ObservableUseCase<List<Project>, Nothing>(postExecutionThread){
+    override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectsRepository.getProjects()
+    }
+
+}
