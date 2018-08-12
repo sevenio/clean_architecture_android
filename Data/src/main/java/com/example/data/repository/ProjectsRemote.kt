16d4f349d1fab63ec8de4365461cb0b7e82ad402
sevenio <seveniotacheon@gmail.com>
2018-08-12 11:11:12 +0530

    create external source interfaces for data layer

---
+++ Data/src/main/java/com/example/data/repository/ProjectsRemote.kt
@@ -0,0 +1,8 @@
+package com.example.data.repository
+
+import com.example.data.model.ProjectEntity
+import io.reactivex.Observable
+
+interface ProjectsRemote {
+    fun getProjects(): Observable<List<ProjectEntity>>
+}
