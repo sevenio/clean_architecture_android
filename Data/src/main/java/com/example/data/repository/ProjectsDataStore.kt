8ef602ebab8f2912d4a739e20f7755681c85191d
sevenio <seveniotacheon@gmail.com>
2018-08-12 11:52:53 +0530

    create data layer store interface

---
+++ Data/src/main/java/com/example/data/repository/ProjectsDataStore.kt
@@ -0,0 +1,12 @@
+package com.example.data.repository
+
+import com.example.data.model.ProjectEntity
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+interface ProjectsDataStore {
+    fun getProjects(): Observable<List<ProjectEntity>>
+    fun getBookmarkedProjects(): Observable<List<ProjectEntity>>
+    fun setProjectAsBookmarked(projectId: String): Completable
+    fun setProjectAsUnBookmarked(projectId: String): Completable
+}
