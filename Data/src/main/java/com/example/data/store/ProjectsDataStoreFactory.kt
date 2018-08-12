6753da2289c36dcfa0df4453877ed7435839062a
sevenio <seveniotacheon@gmail.com>
2018-08-12 12:38:52 +0530

    create data store factory

---
+++ Data/src/main/java/com/example/data/store/ProjectsDataStoreFactory.kt
@@ -0,0 +1,19 @@
+package com.example.data.store
+
+import com.example.data.repository.ProjectsDataStore
+import com.sun.org.apache.xpath.internal.operations.Bool
+import javax.inject.Inject
+
+class ProjectsDataStoreFactory @Inject constructor(private val projectsCacheDataStore: ProjectsCacheDataStore, private val projectsRemoteDataStore: ProjectsRemoteDataStore) {
+    open fun getDataStore(areProjectsCached: Boolean, isCacheExpired: Boolean): ProjectsDataStore {
+        return if (areProjectsCached && !isCacheExpired) {
+            projectsCacheDataStore
+        } else {
+            projectsRemoteDataStore
+        }
+    }
+
+    open fun getCacheDataStore(): ProjectsDataStore {
+        return projectsCacheDataStore
+    }
+}
