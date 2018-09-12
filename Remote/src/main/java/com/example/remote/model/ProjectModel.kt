68c665c243fc36746905190f16e8bf3080b347ad
sevenio <seveniotacheon@gmail.com>
2018-09-12 19:03:06 +0530

    create data models for remote layer

---
+++ Remote/src/main/java/com/example/remote/model/ProjectModel.kt
@@ -0,0 +1,9 @@
+package com.example.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+class ProjectModel(val name: String,
+                   @SerializedName("full_name") val fullName: String,
+                   @SerializedName("startgazers_count") val startCount: Int,
+                   @SerializedName("created_at") val dateCreated: String,
+                   val owner: OwnerModel)
---
+++ Remote/src/main/java/com/example/remote/model/ProjectResponseModel.kt
@@ -0,0 +1,3 @@
+package com.example.remote.model
+
+class ProjectResponseModel(val items: List<ProjectModel>)
