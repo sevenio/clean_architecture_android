f7c3c50ad926f029681ca64fcaebb8437092c73e
sevenio <seveniotacheon@gmail.com>
2018-09-12 22:09:48 +0530

    create remote source implementation

---
+++ Remote/src/main/java/com/example/remote/ProjectRemoteImpl.kt
@@ -0,0 +1,24 @@
+package com.example.remote
+
+import com.example.data.model.ProjectEntity
+import com.example.data.repository.ProjectsRemote
+import com.example.remote.mapper.ModelMapper
+import com.example.remote.model.ProjectModel
+import com.example.remote.service.GitHubTrendingServiceFactory
+import com.example.remote.service.GithubTrendingService
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class ProjectRemoteImpl @Inject constructor(
+        private val service: GithubTrendingService,
+        private val mapper: ModelMapper<ProjectModel, ProjectEntity>
+) : ProjectsRemote {
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return service.searchRepositories("language:kotlin", "stars", "desc")
+                .map {
+                    it.items.map {
+                        mapper.mapFromModel(it)
+                    }
+                }
+    }
+}
