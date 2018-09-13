f2682b9fbdfbfc1eba9a19e4d46adcdc8ea90900
sevenio <seveniotacheon@gmail.com>
2018-09-13 16:51:53 +0530

    create cached project Data access object

--- Cache/src/main/java/com/example/cache/model/CachedProject.kt
+++ Cache/src/main/java/com/example/cache/model/CachedProject.kt
@@ -1,11 +1,14 @@
 package com.example.cache.model
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import com.example.cache.db.ProjectConstants
 
 @Entity(tableName = ProjectConstants.TABLE_NAME)
 data class CachedProject(
         @PrimaryKey
+        @ColumnInfo(name =ProjectConstants.COLUMN_PROJECT_ID)
         var id: String,
         var name: String,
         var fullName: String,
@@ -13,6 +16,7 @@ data class CachedProject(
         var dateCreated: String,
         var ownerName: String,
         var ownerAvatar: String,
+        @ColumnInfo(name = ProjectConstants.COLUMN_IS_BOOKMARKED)
         var isBookmarked: Boolean
 )
 
--- Cache/src/main/java/com/example/cache/model/ProjectConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cache.model
-
-object ProjectConstants {
-
-    const val TABLE_NAME = "projects"
-}
