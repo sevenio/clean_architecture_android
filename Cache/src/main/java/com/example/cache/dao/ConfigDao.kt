650bf3aaaa8a00121e04171605736e6e892913be
sevenio <seveniotacheon@gmail.com>
2018-09-15 03:42:38 +0530

    create projects cache data store implementation

--- Cache/src/main/java/com/example/cache/dao/ConfigDao.kt
+++ Cache/src/main/java/com/example/cache/dao/ConfigDao.kt
@@ -6,13 +6,14 @@ import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import com.example.cache.db.ConfigConstants
 import com.example.cache.model.Config
+import io.reactivex.Flowable
 
 
 @Dao
 abstract class ConfigDao {
 
     @Query(ConfigConstants.QUERY_CONFIG)
-    abstract fun getConfig(): Config
+    abstract fun getConfig(): Flowable<Config>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertConfig(config: Config)
