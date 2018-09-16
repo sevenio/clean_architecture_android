af4d4dac98f710d56cb2aaa28e11ce2b06665db5
sevenio <seveniotacheon@gmail.com>
2018-09-16 23:46:18 +0530

    create presentation state model

---
+++ Presentation/src/main/java/com/example/presentation/state/Resource.kt
@@ -0,0 +1,6 @@
+package com.example.presentation.state
+
+class Resource<out T> constructor(val status: ResourceState,
+                                  val data: T?,
+                                  val message: String?) {
+}
