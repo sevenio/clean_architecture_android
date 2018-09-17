10bb8d7a4e8f774334939af29f1559e8212c1f4c
sevenio <seveniotacheon@gmail.com>
2018-09-17 18:48:29 +0530

    test browse bookmarked projects view model implementation

--- Domain/src/main/java/com/example/domain/interactor/browse/GetBookmarkedProjects.kt
+++ Domain/src/main/java/com/example/domain/interactor/browse/GetBookmarkedProjects.kt
@@ -7,7 +7,7 @@ import com.example.domain.repository.ProjectsRepository
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class GetBookmarkedProjects @Inject constructor(private val projectsRepository: ProjectsRepository, postExecutionThread: PostExecutionThread) : ObservableUseCase<List<Project>, Nothing>(postExecutionThread) {
+open class GetBookmarkedProjects @Inject constructor(private val projectsRepository: ProjectsRepository, postExecutionThread: PostExecutionThread) : ObservableUseCase<List<Project>, Nothing?>(postExecutionThread) {
     public override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
         return projectsRepository.getBookmarkedProjects()
     }
