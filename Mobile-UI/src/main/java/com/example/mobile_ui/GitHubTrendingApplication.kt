5841378ca4bc8c1b9e16a7404c5761a1c7706500
sevenio <seveniotacheon@gmail.com>
2018-09-17 20:16:31 +0530

    setup the application class

---
+++ Mobile-UI/src/main/java/com/example/mobile_ui/GitHubTrendingApplication.kt
@@ -0,0 +1,15 @@
+package com.example.mobile_ui
+
+import android.app.Application
+import timber.log.Timber
+
+class GitHubTrendingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber(){
+        Timber.plant(Timber.DebugTree())
+    }
+}
