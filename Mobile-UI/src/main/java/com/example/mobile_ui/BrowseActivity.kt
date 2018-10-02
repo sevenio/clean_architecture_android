f0ae0d7ceda3e140e956dd758a1df4ec9ecefd1e
sevenio <seveniotacheon@gmail.com>
2018-10-02 21:55:07 +0530

    setup browse projects activity

--- Mobile-UI/src/main/java/com/example/mobile_ui/BrowseActivity.kt
+++ Mobile-UI/src/main/java/com/example/mobile_ui/BrowseActivity.kt
@@ -3,20 +3,22 @@ package com.example.mobile_ui
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class BrowseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setupBrowseRecycler()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    }
+
+    private fun setupBrowseRecycler() {
+        recycler_view.layoutManager = LinearLayoutManager(this)
     }
 
 }
