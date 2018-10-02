ea002022c80f8500e777ff378637bd208167a1f2
sevenio <seveniotacheon@gmail.com>
2018-10-02 16:43:29 +0530

    create ui module's project representation

--- Mobile-UI/src/main/java/com/example/mobile_ui/UIThread.kt
+++ Mobile-UI/src/main/java/com/example/mobile_ui/UIThread.kt
@@ -5,7 +5,7 @@ import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
-class UIThread @Inject constructor(): PostExecutionThread {
+class UIThread @Inject constructor() : PostExecutionThread {
     override val scheduler: Scheduler
         get() = AndroidSchedulers.mainThread()
 }
