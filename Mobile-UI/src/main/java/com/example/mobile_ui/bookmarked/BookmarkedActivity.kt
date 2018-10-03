cc4bfe04671fa8b28bca49f8e292b81ee8ff6977
sevenio <seveniotacheon@gmail.com>
2018-10-03 22:21:09 +0530

    create browse bookmarked projects adapter

---
+++ Mobile-UI/src/main/java/com/example/mobile_ui/bookmarked/BookmarkedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobile_ui.bookmarked
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BookmarkedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmarked)
+    }
+}
